8d91aaf08cd9a6d082b864d68c7ad720128bf674
Jason Rai <jrai@homex.com>
2019-08-07 15:58:52 -0400

    updated with second example
    also seems like textScaleFactorTestValue is different for different text?????

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String text;
+
+  const MyApp({Key key, this.text}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,34 +14,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(text: text),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.text}) : super(key: key);
 
-  final String title;
+  final String text;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Demo"),
       ),
       body: Center(
         child: Column(
@@ -49,24 +45,15 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 children: <Widget>[
                   Text(
-                    'You have pushed the button this many times:',
+                    widget.text,
                     maxLines: 1,
                     softWrap: false,
                   ),
                 ],
               ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -6,32 +6,68 @@ import 'package:flutter_app/main.dart';
 // This file demonstrates the text scaling bug
 // There is no overflow in `MyApp` when ran normally with `flutter run`
 void main() {
+  String testText1 =
+      "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+  String testText2 = "You have pushed the button this many times:";
+
+  double textScaleFactorTestValue1 = 0.337;
+  double textScaleFactorTestValue2 = 0.49;
+
   // fails when ran with 'flutter test'
   // passes when ran with 'flutter run test/widget_test.dart'
-  testWidgets('Test layout', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+  testWidgets('Test layout 1', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp(text: testText1));
+  });
+
+  // fails when ran with 'flutter test'
+  // passes when ran with 'flutter run test/widget_test.dart'
+  testWidgets('Test layout 2', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp(text: testText2));
   });
 
   // passes when ran with 'flutter test'
   // passes when ran with 'flutter run test/widget_test.dart'
-  testWidgets('Test layout with workaround', (WidgetTester tester) async {
+  testWidgets('Test layout 1 with workaround', (WidgetTester tester) async {
     final TestWidgetsFlutterBinding binding =
         TestWidgetsFlutterBinding.ensureInitialized();
-    binding.window.textScaleFactorTestValue = 0.49;
+    binding.window.textScaleFactorTestValue = textScaleFactorTestValue1;
 
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(text: testText1));
   });
 
   // passes when ran with 'flutter test'
   // passes when ran with 'flutter run test/widget_test.dart'
-  testWidgets('Test layout with workaround, only when needed',
+  testWidgets('Test layout 2 with workaround', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+    binding.window.textScaleFactorTestValue = textScaleFactorTestValue2;
+
+    await tester.pumpWidget(MyApp(text: testText2));
+  });
+
+  // passes when ran with 'flutter test'
+  // passes when ran with 'flutter run test/widget_test.dart'
+  testWidgets('Test layout 1 with workaround, only when needed',
       (WidgetTester tester) async {
     final TestWidgetsFlutterBinding binding =
         TestWidgetsFlutterBinding.ensureInitialized();
     if (Platform.environment.containsKey('FLUTTER_TEST')) {
-      binding.window.textScaleFactorTestValue = 0.49;
+      binding.window.textScaleFactorTestValue = textScaleFactorTestValue1;
     }
 
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(text: testText1));
+  });
+
+  // passes when ran with 'flutter test'
+  // passes when ran with 'flutter run test/widget_test.dart'
+  testWidgets('Test layout 2 with workaround, only when needed',
+      (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      binding.window.textScaleFactorTestValue = textScaleFactorTestValue2;
+    }
+
+    await tester.pumpWidget(MyApp(text: testText2));
   });
 }
